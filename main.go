824064c89c7af1c27d1bdf9ea412e1f88b2333f9
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-18 16:19:48 +0200

    feat: Add PREFFIX support

--- main.go
+++ main.go
@@ -9,8 +9,14 @@ import (
 func main() {
 	hostname, _ := os.Hostname()
 
+	prefix := ""
+	prefixEnv := os.Getenv("PREFIX")
+	if prefixEnv != "" {
+		prefix = prefixEnv + " "
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1><center>Hello PSS.sk! %s</center></h1>\n", hostname)
+		fmt.Fprintf(w, "<h1><center>%sHello PSS.sk! %s</center></h1>\n", prefix, hostname)
 	})
 
 	fmt.Println("Listen on 0.0.0.0:8000, see: http://127.0.0.1:8000")
